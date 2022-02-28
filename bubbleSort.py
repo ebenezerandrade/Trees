dac143cf112d5a4efa585e5c4ae4156810c7b0e6
Ebenezer Andrade <ebenezer.andrade1@gmail.com>
2022-02-28 15:23:06 -0300

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,6 +1,6 @@
 def bubbleSort(arr):
   n = len(arr)
-  for j in range(n-1):
+  for j in range(n):
     for i in range(n-1):
       if(arr[i] > arr[i+1]):
         arr[i], arr[i+1] = arr[i+1], arr[i]
